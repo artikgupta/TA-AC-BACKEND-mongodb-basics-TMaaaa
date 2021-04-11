ea53dfe15e24f072d1d97bb3e8d74de246f1a704
artikgupta <artikgupta>
2021-04-11 12:41:40 +0530

    completed CRUD assignment

---
+++ block-BMaaag/index.js
@@ -0,0 +1,8 @@
+[
+    {name: 'Dhauldhar range', height: '4000 mtrs'},
+    {name: 'Himalaya range', height: '5000 mtrs'},
+    {name: 'Panjal range', height: '8000 mtrs'},
+    {name: 'Karakoram range', height: '9000 mtrs'},
+    {name: 'Purvanchal range', height: '44000 mtrs'}
+
+]
